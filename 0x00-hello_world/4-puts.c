8d7a979ccc56823fa6a017f46e19a4203223af99
Engy-Hany <engyhany925@gmail.com>
2023-03-18 15:38:02 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * Description: Write a C program that prints exactly Programming is like building a multilingual puzzle
  *return: 0 (sucess)
  */
 int main(void)
