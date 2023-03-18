6fe998fd0b7ec3172256db7510ddc645876b605e
Engy-Hany <engyhany925@gmail.com>
2023-03-18 15:34:47 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*
- * main - Aprogram that print a line with puts function*
+/**
+ * main - Entry point
  *return: 0 (sucess)
- **/
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
